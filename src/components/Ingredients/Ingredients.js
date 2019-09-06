83c52b04347b4aa06c48a54118338c53ccf78976
Zolo <a.burtsev@hostland.ru>
2019-09-06 18:07:25 +0300

    initialization

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import Search from './Search';
+import IngredientList from './IngredientList';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-project-a0bf9.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setUserIngredients(prevState => [
+          ...prevState,
+          { id: data.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = () => {
+    console.log('remove');
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
